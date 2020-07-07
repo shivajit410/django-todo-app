3f498bc8c3d9edf9587b1921d56d794a628ce2d1
Shivaji Thattikota <shivajithattikota@outlook.com>
2020-07-07 18:46:33 +0530

    CRUD operations

--- todo_root/tasks/views.py
+++ todo_root/tasks/views.py
@@ -1,6 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import  redirect, render
 from django.http import HttpResponse
+from .models import Task
+from .forms import TaskForm
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    tasks = Task.objects.all()
+    form = TaskForm
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+    context = {'tasks' : tasks, 'form':form}
+    return render(request, 'tasks/list.html', context)
+
+
+def updateTask(request, pk):
+    task = Task.objects.get(pk=pk)
+
+    form = TaskForm(instance=task)
+    context = {'form':form}
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    return render(request, 'tasks/update_task.html', context)
+
+def deleteTask(request, pk):
+    item = Task.objects.get(pk=pk)
+
+    if request.method == 'POST':
+        item.delete()
+        return redirect('/')
+    context = {'item':item}
+    return render(request, 'tasks/delete.html', context)
