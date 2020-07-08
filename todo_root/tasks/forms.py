6c28a5189e6e1f97dba6d74fac0a0e5cfa29d643
Shivaji Thattikota <shivajithattikota@outlook.com>
2020-07-08 13:02:22 +0530

    added placeholder value

--- todo_root/tasks/forms.py
+++ todo_root/tasks/forms.py
@@ -3,8 +3,14 @@ from django.forms import ModelForm
 
 from .models import Task
 
+
 class TaskForm(forms.ModelForm):
+
+    title = forms.CharField(
+        max_length=255, widget=forms.TextInput(attrs={"placeholder": "Add new task..."})
+    )
 
     class Meta:
         model = Task
-        fields = '__all__'
+        fields = "__all__"
+
